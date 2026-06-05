27e9c61c51cd9ba39cb8943944b5d3509f110fd7
nadir <nadir.younees@gmail.com>
2026-06-05 11:21:42 +0100

    first commit2

--- blog/views.py
+++ blog/views.py
@@ -1,40 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from .models import Post
-from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-
-# def post_list(request):
-#     posts = Post.objects.all()
-#     return render(request, "home.html", {"posts": posts})
-
-
-# def post_detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     return render(request, "post_detail.html", {"post": post})
-
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = "home.html"
     context_object_name = "posts"
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "post_detail.html"
 
-class BlogCreateView(CreateView): # new
+class BlogCreateView(LoginRequiredMixin, CreateView):  # new
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
 
-class BlogUpdateView(UpdateView): # new
+class BlogUpdateView(LoginRequiredMixin, UpdateView):  # new
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
 
-class BlogDeleteView(DeleteView): # new
+class BlogDeleteView(LoginRequiredMixin, DeleteView):  # new
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
--- django_project/settings.py
+++ django_project/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$*i%l_qy&_^@0k$tck4&_(2g_4ar979=5+9)ra0(5qp9q#!j27"
+SECRET_KEY = "testingkey"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-43dd2.up.railway.app",
+    "https://web-production-b450.up.railway.app/",
 ]
 
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
